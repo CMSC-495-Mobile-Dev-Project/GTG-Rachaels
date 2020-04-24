91ab6d8bf1503f5bba7af49602e6404b873bd237
Rachael Harner <62813101+rharner87@users.noreply.github.com>
2020-04-24 15:06:27 -0600

    Update MainComponent.js

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -82,17 +82,10 @@ const MainNavigator = createDrawerNavigator({
 
 class Main extends Component {
     render() {
-        return ( <
-            View style = {
-                {
-                    flex: 1,
-                    paddingTop: Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight
-                }
-            } >
+        return ( <View style = {     {      flex: 1,     paddingTop: Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight             }         } >
             <
             MainNavigator / >
-            <
-            /View>
+            </View>
         );
     }
 }
