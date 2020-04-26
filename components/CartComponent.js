766315b0a16f0785031c80708990e78a7a95ff19
Rachael Harner <62813101+rharner87@users.noreply.github.com>
2020-04-26 15:15:27 -0600

    Update CartComponent.js

--- components/CartComponent.js
+++ components/CartComponent.js
@@ -1,331 +1,213 @@
 import React, { Component } from 'react';
-
 import { Image, ScrollView, FlatList, View, Text, StyleSheet, Dimensions, TouchableOpacity, Alert } from 'react-native';
-
-// import {Kittehs} from '../images/kittehs.jpg';
-
-
+import Checkout from './CheckoutComponent';
 
 var {height} = Dimensions.get('window');
 
-
-
 class Cart extends Component {
 
-
-
     static navigationOptions = {
-
         title: 'Cart'
-
     }
-
     
-
     constructor(props) {
-
         super(props);
-
         this.state = {
-
           modalVisible:false,
-
           userSelected:[],
-
           data: [
-
-            {id:1,  name: "Eggs", quantity: 1,image:"https://www.incnow.com/wp-content/uploads/2013/03/egg-carton.jpg",    },
-
-            {id:2,  name: "Milk", quantity: 1, image:"https://shepherdminiatures.com/wp-content/uploads/2018/02/milk-red-small-copy.jpg",      },
-
-            {id:3,  name: "Bread", quantity: 1, image:"https://americacomesalive.com/i/Wonder-bread.png", } ,
-
-            {id:4,  name: "Cheese", quantity: 1, image:"http://www.diningchicago.com/blog/wp-content/uploads/2012/10/Kraft-cheese.png",    } ,
-
-            {id:5,  name: "Potatoes", quantity: 1, image:"https://www.agric.wa.gov.au/sites/gateway/files/W07_0018_White_star_potato.JPG",    },             
-
-            {id:6,  name: "Cheeese", quantity: 1, image:"http://www.diningchicago.com/blog/wp-content/uploads/2012/10/Kraft-cheese.png",    } 
-
-          ]
-
+            {id:1,  name: "Eggs", quantity: 1, price: 1.00, image:"https://www.incnow.com/wp-content/uploads/2013/03/egg-carton.jpg"},
+            {id:2,  name: "Milk", quantity: 0, price: 1.50, image:"https://shepherdminiatures.com/wp-content/uploads/2018/02/milk-red-small-copy.jpg"},
+            {id:3,  name: "Bread", quantity: 0, price: 1.62, image:"https://americacomesalive.com/i/Wonder-bread.png"} ,
+            {id:4,  name: "Cheese", quantity: 0, price: 3.50, image:"http://www.diningchicago.com/blog/wp-content/uploads/2012/10/Kraft-cheese.png"} ,
+            {id:5,  name: "Potatoes", quantity: 0, price: 2.50, image:"https://www.agric.wa.gov.au/sites/gateway/files/W07_0018_White_star_potato.JPG" }, 
+            {id:6,  name: "Butter", quantity: 0,price: 5.13,    image:"https://bmcontent.affino.com/AcuCustom/Sitename/DAM/162/DD4G6wuXUAAMMVf.png"} ,
+            {id:7,  name: "Corn", quantity: 0, price: 4.75,  image:"http://www.pngall.com/wp-content/uploads/2016/05/Corn-PNG-File.png" } ,
+            {id:8,  name: "Chips", quantity: 0, price: 1.99,   image:"https://i.pinimg.com/736x/84/6d/3e/846d3e71415c22c2238af6b927e6b473.jpg" } ,
+            {id:9,  name: "Soda", quantity: 0, price:  1.22,  image:"http://pics.drugstore.com/prodimg/416899/900.jpg"   } ,
+            {id:10, name: "Pizza", quantity: 0, price:7.99,   image:"http://www.cuginospizzas.com/wp-content/uploads/2016/03/Pepperoni-Pizza-PNG-Image.png"} ,            
+            ]
         };
-
     }
-
     clickEventListener = (item) => {
-
         Alert.alert('Adjust Quantities', 'Item: '+ item.name, 
-
-        [{text: '+1', onPress: () => this.setState({[item.quantity]: item.quantity++})},
-
-        {text: '-1', onPress: () => this.setState({[item.quantity]: item.quantity--})},
-
+        [{text: '+1', onPress: () => this.check_quantities_adding(item)},        
+        {text: '-1', onPress: () => this.check_quantities_subtracting(item)},
         {text: 'Cancel', onPress: () => console.log('cancelled'), style: 'cancel'}
-
         ], 
-
         {cancelable: true});
-
     }
 
+    check_quantities_adding=(item)=>{
+      console.log(item.name + item.quantity);
+      if( item.quantity > 0 && item.quantity < 50){   
+        this.setState({[item.quantity]: item.quantity++});
+      }
+      else if (item.quantity == 50) {
+        Alert.alert("Sorry, you've reached the maximum limit of 50.")
+        console.log(item.quantity);
+      }   
+    }
 
-
+    check_quantities_subtracting=(item)=>{      
+      if(item.quantity == 1) {   
+        Alert.alert("Item removed from cart.")
+        //this.splice(id, item.id);
+        {this.state.data.map(item => (
+          <li key={item}>{item}</li>
+        ))}
+        this.onRemoveItem(item.id);        
+        //console.log(this.state.data.indexOf); 
+        //console.log(this.state.data);       
+      }
+      else if (item.quantity > 1) {
+        this.setState({[item.quantity]: item.quantity--});
+      }       
+      //console.log(item.name + item.quantity);
+    }
+    onRemoveItem = id => {
+      this.setState(state => {
+        const data = state.data.filter(item => item.id !== id);
+        console.log(data);
+        return {
+          data,
+        };
+      });
+    };
+    calculateTotal=() => {
+      var total = 0;
+      var cartArray = this.state.data;
+      var index = 0;
+      for (index = 0; index < cartArray.length; index++) {
+        total += (cartArray[index].quantity * cartArray[index].price);
+      }
+      return total.toFixed(2);
+    }
+    calculateItemTotal=(item) => {
+      var itemTotal = item.quantity * item.price;
+      return itemTotal.toFixed(2);
+    }
 
 
     render() {
-
         return (
-
             <View style={styles.container}>
-
                 <FlatList 
-
                 style={styles.scrollStyle}
-
                 columnWrapperStyle={styles.listContainer}
-
                 data={this.state.data}
-
                 keyExtractor= {(item) => {
-
                     return item.id;
-
                 }}
-
                 renderItem={({item}) => {
-
                 return (
-
                     <TouchableOpacity style={styles.card} onPress={() => {this.clickEventListener(item)}}>
-
                     <Image style={styles.image} source={{uri: item.image}}/>
-
                     <View style={styles.cardContent}>
-
                         <Text style={styles.name}>{item.name}</Text>
-
-                        <Text style={styles.count}>{item.count}</Text>
-
+                        <Text style={styles.count}>${this.calculateItemTotal(item)}</Text>                        
                         <TouchableOpacity style={styles.followButton} onPress={()=> this.clickEventListener(item)}>
-
                         <Text style={styles.followButtonText}>Quantity: {item.quantity}</Text>  
-
-                        </TouchableOpacity>
-
+                        </TouchableOpacity>                            
                     </View>
-
                     </TouchableOpacity>
-
                 )}}/>                        
-
-                <FlatList style={styles.flatStyle}>
-
-                    <Text>test</Text> 
-
-                </FlatList>
-
+                <View style={styles.flatStyle}>
+                    <Text></Text>                    
+                    <Text>  Total: ${this.calculateTotal()}</Text>
+                    <TouchableOpacity style={styles.followButton} onPress={() => this.props.navigation.navigate('Checkout')}>
+                    <Text style = {styles.followButtonText}>
+                    Check Out
+                    </Text>
+                    </TouchableOpacity>
+                </View>
              </View> 
-
         );
-
     }
-
 }
-
-
 
 const styles = StyleSheet.create({
-
     container: {
-
         flex: 1,
-
         flexDirection: 'row', 
-
         height: height,
-
         marginLeft: 5,
-
         marginRight: 5,
-
         marginBottom: 5,
-
     },
-
     scrollStyle: {
-
-        //flex: 5, 
-
         width: 600,
-
         backgroundColor: 'white'
-
     },    
-
     flatStyle: {
-
-        //flex: 10,
-
-        width: 200,
-
-        backgroundColor: 'gray'
-
+        textAlign: 'center',
+        width: 120,
+        backgroundColor: 'white'
     },
-
     contentList:{
-
         flex:1,
-
       },
-
       cardContent: {
-
         marginLeft:20,
-
         marginTop:10
-
       },
-
       image:{
-
         width:90,
-
         height:90,
-
         borderRadius:45,
-
         borderWidth:2,
-
         borderColor:"#ebf0f7"
-
       },
-
     
-
       card:{
-
         shadowColor: '#00000021',
-
         shadowOffset: {
-
           width: 0,
-
           height: 6,
-
         },
-
         shadowOpacity: 0.37,
-
         shadowRadius: 7.49,
-
         elevation: 12,
-
     
-
         marginLeft: 20,
-
         marginRight: 20,
-
         marginTop:20,
-
         backgroundColor:"white",
-
         padding: 10,
-
         flexDirection:'row',
-
         borderRadius:30,
-
       },
-
     
-
       name:{
-
         fontSize:18,
-
         flex:1,
-
         alignSelf:'center',
-
         color:"#3399ff",
-
         fontWeight:'bold'
-
       },
-
       count:{
-
         fontSize:14,
-
         flex:1,
-
         alignSelf:'center',
-
         color:"#6666ff"
-
       },
-
       followButton: {
-
         marginTop:10,
-
         height:35,
-
         width:100,
-
         padding:10,
-
         flexDirection: 'row',
-
         justifyContent: 'center',
-
         alignItems: 'center',
-
         borderRadius:30,
-
         backgroundColor: "white",
-
         borderWidth:1,
-
         borderColor:"gray",
-
       },
-
       followButtonText:{
-
         color: "blue",
-
         fontSize:12,
-
       }
-
 })
-
-
-
-
-
-function Cat(props) {
-
-    return (
-
-        <View>
-
-            <Image 
-
-                source={require('../assets/logo.png')}
-
-                style={{width: 200, height: 200}}/>
-
-            <Text>Quantity: {props.quantity}</Text>
-
-        </View>    
-
-    );
-
-}
-
-
 
 export default Cart;
